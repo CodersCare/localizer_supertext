261a122931ab64fb39c18c15aeced34ea3514303
Jo Hasenau <info@cybercraft.de>
2022-07-22 15:31:26 +0200

    [TASK] Add PHP CS Fixer and GitLab CI

--- Configuration/TCA/Overrides/tx_localizer_settings.php
+++ Configuration/TCA/Overrides/tx_localizer_settings.php
@@ -1,11 +1,11 @@
 <?php
+
 if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
+    die('Access denied.');
 }
 
 $GLOBALS['TCA']['tx_localizer_settings']['columns']['type']['config']['items'][] = [
-    'LLL:EXT:localizer_supertext/Resources/Private/Language/locallang_db.xlf:tx_localizer_settings.type.I.localizer_supertext', 'localizer_supertext'
+    'LLL:EXT:localizer_supertext/Resources/Private/Language/locallang_db.xlf:tx_localizer_settings.type.I.localizer_supertext', 'localizer_supertext',
 ];
-
 
 $GLOBALS['TCA']['tx_localizer_settings']['types']['localizer_supertext']['showitem'] = 'hidden, --palette--;;1, type, title, description, url, username, password;LLL:EXT:localizer_supertext/Resources/Private/Language/locallang_db.xlf:tx_localizer_settings.password, --palette--;;2, --palette--;;3, l10n_cfg, source_locale, target_locale';
